307d962e231bfbb606cb046e4f2d820ed7f84f55
âBrank-Lan <2055929044@qq.com>
2021-02-20 16:13:59 +0800

    deploy

---
+++ precache-manifest.fc13518e0cfd9d3a2fdd208e1df782b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aaaa1b3cf2b6d860055",
+    "url": "/morney-dist/css/app.e572fc60.css"
+  },
+  {
+    "revision": "41da926bf295177e712c",
+    "url": "/morney-dist/css/chunk-vendors.5014d46d.css"
+  },
+  {
+    "revision": "bfad97ebd7841b4cc4ee79173a7d6707",
+    "url": "/morney-dist/index.html"
+  },
+  {
+    "revision": "0aaaa1b3cf2b6d860055",
+    "url": "/morney-dist/js/app.81d9a849.js"
+  },
+  {
+    "revision": "41da926bf295177e712c",
+    "url": "/morney-dist/js/chunk-vendors.1aea3bc3.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-dist/robots.txt"
+  }
+]);
